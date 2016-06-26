3fa3111b5d6e61c7dcfdd060e364c9e448df0edd
Jit Ray Chowdhury <jit@aurobots.com>
2016-06-25 20:10:41 -0700

    sperated point cloud generation and mapping and script to publish params and output command

---
+++ ros/src/autoware_jit_launch/script/mapping_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rostopic pub /config/ndt_mapping runtime_manager/ConfigNdtMapping "header:
+  seq: 0
+  stamp: {secs: 0, nsecs: 0}
+  frame_id: ''
+resolution: 1.0
+step_size: 0.1
+trans_eps: 0.01
+leaf_size: 5.0"
---
+++ ros/src/autoware_jit_launch/script/mapping_save.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rostopic pub /config/ndt_mapping_output runtime_manager/ConfigNdtMappingOutput "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: ''
+filename: '/media/fast_data/ros_sandbox/Autoware/ros/out.pcd'
+filter_res: 0.0"
